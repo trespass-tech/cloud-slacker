7ec8bf4217c83f1a575711996f9af948f0d913b0
Dmitry Livchak <dmitry.livchak@gmail.com>
2018-05-01 19:02:30 +0100

    Dummy commit

--- handlers/code-build.js
+++ handlers/code-build.js
@@ -14,6 +14,7 @@ module.exports.notify = (event, context, callback) => {
       color = '';
   }
 
+  
   const buildArn = event.detail['build-id'];
   const buildId = buildArn.substring(buildArn.indexOf(':build/') + 7);
 
